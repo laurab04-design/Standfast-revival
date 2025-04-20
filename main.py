5914b87758160b166d1f97ccec6ed7d72910411e
laurab04-design <laura_b_04@hotmail.com>
2025-04-20 11:24:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from fossedata_core import upload_to_drive
 from fastapi import FastAPI
 from standfast_revival import fetch_archived_urls, scrape_show_results
+from standfast_revival.golden_judges_scraper import fetch_golden_judges
+import asyncio
 
 app = FastAPI()
 
@@ -18,3 +21,9 @@ def run():
     urls = fetch_archived_urls()
     scrape_show_results(urls)
     return {"message": "Scrape run complete"}
+
+@app.get("/judges")
+def scrape_judges():
+    asyncio.run(fetch_golden_judges())
+    upload_to_drive("golden_judges.json", "application/json")
+    return {"message": "Golden Retriever judge list updated and uploaded!"}
