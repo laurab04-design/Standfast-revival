0dd19db75a1a9c5ed02db747bb6e4f7ed8de2641
laurab04-design <laura_b_04@hotmail.com>
2025-04-20 12:18:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,6 @@ app = FastAPI()
 def root():
     return {"message": "Welcome to the Standfast Revival API"}
 
-@app.get("/scrape")
-def scrape():
-    urls = fetch_archived_urls()
-    scrape_show_results(urls)
-    return {"message": "Scraping completed!"}
-
-@app.get("/run")
-def run():
-    urls = fetch_archived_urls()
-    scrape_show_results(urls)
-    return {"message": "Scrape run complete"}
-
 @app.get("/judges")
 def scrape_judges():
     asyncio.run(fetch_golden_judges())
